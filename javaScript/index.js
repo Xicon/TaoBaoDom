b0aeb5ec60a52639361217e3f1669d1be57bcc04
7car <2572967565@qq.com>
2020-10-14 14:55:57 +0800

    0.1

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,22 +1,44 @@
-let aside = document.querySelector('.aside').querySelectorAll('li'),
-    a_len = aside.length,
+let
+    aside = document.querySelector('.aside').querySelectorAll('li'),
+    aside_len = aside.length,
     asideBox = document.querySelector('.asideBox').querySelectorAll('.asideItem'),
-    m_len = asideBox.length,
+    asideItem_len = asideBox.length,
     swiper_wrapper = document.querySelector('.swiper-wrapper')
-for (let i = 0; i < a_len; i++) {
+
+
+for (let i = 0; i < aside_len; i++) {
     aside[i].addEventListener('mouseover', function () {
         aside[i].setAttribute('index', i);
         let index = this.getAttribute('index');
-        for (let j = 0; j < m_len; j++) {
+        for (let j = 0; j < asideItem_len; j++) {
             asideBox[j].style.display = "none";
         }
         asideBox[index].style.display = "block";
         swiper_wrapper.style.display = "none";
     })
     aside[i].addEventListener('mouseout', function () {
-        for (let j = 0; j < m_len; j++) {
+        for (let j = 0; j < asideItem_len; j++) {
             asideBox[j].style.display = "none";
         }
         swiper_wrapper.style.display = "block";
+    })
+}
+let m_Item = document.querySelector('.maninContentRightItemBox').querySelectorAll('li'),
+    m_Item_len = m_Item.length,
+    m_list = document.querySelector('.maninContentRightItemBannerBox').querySelectorAll('li'),
+    m_list_len = m_list.length;
+
+for (let i = 0; i < m_Item_len; i++) {
+    m_Item[i].addEventListener('mouseover', function () {
+        for (let i = 0; i < m_Item_len; i++) {
+            m_Item[i].className = ""
+        }
+        this.className = "maninContentRightItemDom";
+        m_Item[i].setAttribute('index', i)
+        let index = this.getAttribute('index')
+        for (let j = 0; j < m_list_len; j++) {
+            m_list[j].style.display = "none";
+        }
+        m_list[index].style.display = "block";
     })
 }
